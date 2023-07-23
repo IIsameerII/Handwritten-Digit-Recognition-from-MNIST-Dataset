b18afacc20608529a4d645774d697bd2c23f0f85
IIsameerII <88422517+IIsameerII@users.noreply.github.com>
2023-07-23 19:49:19 +0530

    Added IF condition to check None Type for snip

--- main.py
+++ main.py
@@ -23,14 +23,10 @@ st.success("""
 Draw on the canvas, get the digits predicted with the confidence scores!
 """)
 
-# Specify brush parameters and drawing mode
-b_width = st.slider("Brush width: ", 1, 10, 10)
-
-
 st.header("Drawable Canvas")
 
 # Create a canvas component
-image_data = st_canvas(stroke_width=b_width,
+image_data = st_canvas(stroke_width=10,
                        stroke_color='#000000',
                        background_color='#FFFFFF',
                        width=700,
@@ -49,51 +45,56 @@ if image_data is not None:
 
     
     for snip in digit_lis:
+
+        # Sometimes if we put a dot in the canvas or if the 
+        # Canvas in empty on streamlit the NoneType Error is
+        # generated. To check that this if condition is placed
+        if snip is not None:
         
-        pil_image = Image.fromarray(snip)       
-        
-        # Transfrom the image to fit the model inputs
-        transform = transforms.Compose([
-            transforms.Resize(size=(28,28)),
-            transforms.ToTensor(),
-        ])
-
-        # transform the image
-        transformed_image = transform(pil_image).to(device)
-
-        # Unsqueeze the tensor to [1,1,28,28]
-        transformed_image = torch.unsqueeze(transformed_image,dim=0)
-
-        with torch.inference_mode():
-            # Set the model to evaluation mode
-            model.eval()
+            pil_image = Image.fromarray(snip)       
             
-            # Predit the digit of the model
-            y_logits = model(transformed_image)
-            y_pred_prob = torch.softmax(y_logits,dim=1)
-            
-            conf = torch.max(y_pred_prob).item()*100
-            digit = str(torch.argmax(y_pred_prob).item())
+            # Transfrom the image to fit the model inputs
+            transform = transforms.Compose([
+                transforms.Resize(size=(28,28)),
+                transforms.ToTensor(),
+            ])
 
-        invert = ip.invert_colors_opencv(pil_image)
+            # transform the image
+            transformed_image = transform(pil_image).to(device)
 
-        with st.container():
-            col1,col2 = st.columns(2)
+            # Unsqueeze the tensor to [1,1,28,28]
+            transformed_image = torch.unsqueeze(transformed_image,dim=0)
 
-            with col1:
-                st.image(invert,caption=f'Prediction: {digit}  |  Confidence: {conf:.2f}')
+            with torch.inference_mode():
+                # Set the model to evaluation mode
+                model.eval()
+                
+                # Predit the digit of the model
+                y_logits = model(transformed_image)
+                y_pred_prob = torch.softmax(y_logits,dim=1)
+                
+                conf = torch.max(y_pred_prob).item()*100
+                digit = str(torch.argmax(y_pred_prob).item())
 
-            with col2:
-                y_pred_prob_numpy = y_pred_prob.squeeze().to('cpu').numpy()
+            invert = ip.invert_colors_opencv(pil_image)
 
-                chart_data = pd.DataFrame(
-                    y_pred_prob_numpy,
-                    [0,1,2,3,4,5,6,7,8,9]                
-                )
+            with st.container():
+                col1,col2 = st.columns(2)
 
-                chart_data.columns = ['Confidence']
+                with col1:
+                    st.image(invert,caption=f'Prediction: {digit}  |  Confidence: {conf:.2f}')
 
-                st.bar_chart(chart_data)
+                with col2:
+                    y_pred_prob_numpy = y_pred_prob.squeeze().to('cpu').numpy()
+
+                    chart_data = pd.DataFrame(
+                        y_pred_prob_numpy,
+                        [0,1,2,3,4,5,6,7,8,9]                
+                    )
+
+                    chart_data.columns = ['Confidence']
+
+                    st.bar_chart(chart_data)
 
         
         
